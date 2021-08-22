76a2cbfb6e987510a5c7b9c06b106e7695162708
CARLOS PARRA <carlosparra@tec.mx>
2021-08-22 04:58:54 +0000

    corrección

--- assignments/14DistanciaDosPuntos/tests/test_exercise.py
+++ assignments/14DistanciaDosPuntos/tests/test_exercise.py
@@ -8,7 +8,6 @@ from tests.input_data import input_values
 def test_exercise(value, result, message):
     """
     Tests the code in src.exercise using the inputs from tests.input_data. Asserts that what is printed in the terminal is the same as the expected result.
-
     Parameters:
         value: list(str)
             The values that are used as input for the test. Usually strings, since we are simulating input from user.
@@ -22,7 +21,6 @@ def test_exercise(value, result, message):
     def mock_input(input_s=None):
         """
         Create a mock input, where each input is obtained from the list of inputs. Appends the input to a list.
-
         Parameters:
             input_s: str
                 Input from the test exercise.
